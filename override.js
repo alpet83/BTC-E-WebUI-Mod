00aa6374e6d8b78d29a7d8e0bd6a15c77a1c4818
Alexander Petrov <alpet1@gmail.com>
2017-09-21 01:48:35 +0300

    Embedding DOM chart (graphical depth)

--- override.js
+++ override.js
@@ -21,14 +21,46 @@ html = html.replace('allowtransparency="true"', 'allowtransparency="false"');
 // html = html.replace("style=1", "style=3");
 html = html.replace('width="661" height="300"', 'width="' + cfw + '" height="' + fh + '"');
 
+
 e.innerHTML = html;
+
+
 $("#content").children('div').children('div:first').appendTo( $("#content").children('div').eq(1) );
 var dom = $(".inblock:first");
 dom.width(450).css("background-color", "#c0ffc0"); 
 dom = dom.next();
 dom.css("background-color", "#FFC0C0");
 dom.width(450);
-$("#orders-self-list").parent().css("float", "left").insertAfter($(".inblock:first") ).width(cfw - 950); 
+var orders_list = $("#orders-self-list");
+
+olw = cfw - 950;
+
+orders_list.parent().css("float", "left").insertAfter($(".inblock:first") ).width(olw); 
+
+pair = $(".pairs-selected:first").get(0).innerHTML.match(/(\w+)\/(\w+)/);
+
+var dom_chart_url = "???";
+
+// COMMENT next block, for disable DOM chart
+if (pair && pair[1])
+{
+   pair = (pair[1] + '_' + pair[2]).toLowerCase();
+   // html = orders_list.parent().innerHTML;
+   dom_chart_url = 'https://vds.alpet.me/draw_depth.php?pair=' + pair + '&w=' + olw + '&limit=150';
+   setTimeout(reloadDOMChart, 60000);
+   orders_list.after('<img id="dom_chart" src="' + dom_chart_url + '" />');
+}   
+
+function reloadDOMChart()
+{
+    var d = new Date(); 
+    $("#dom_chart").attr("src", dom_chart_url + "&"+d.getTime());     
+    setTimeout(reloadDOMChart, 60000);
+}
+
+
+
+
 
 var btns = $(".pairs:first");
 if (btns) {
